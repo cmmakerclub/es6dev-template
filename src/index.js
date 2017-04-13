378e2793dba1e8f0bc7841c5bea30da95dadf8b8
Nat <nat@cmmc.io>
2017-04-13 17:05:35 +0700

    add example.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+const DELAY_TIME_MS = 1000
+console.log(`DELAY_TIME_MS = ${DELAY_TIME_MS}`)
+
+setInterval(() => {
+  console.log(`Hello world: ${new Date().getTime()}`)
+}, DELAY_TIME_MS)
